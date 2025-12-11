f434ee60251d7aae9e19c55d1c01c47119d05362
imperfect-12 <b.prashant@iitg.ac.in>
2025-12-11 13:59:24 +0530

    create the books route, add books, show book in home

---
+++ models/book.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const path = require("path");
+
+const coverImageBasePath = "uploads/bookCovers";
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    publishDate: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    pageCount: {
+      type: Number,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    coverImageName: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Author",
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+bookSchema.virtual("coverImagePath").get(function () {
+  if (this.coverImageName != null) {
+    return `/${coverImageBasePath}/${this.coverImageName}`;
+  }
+});
+
+module.exports = mongoose.model("Book", bookSchema);
+module.exports.coverImageBasePath = coverImageBasePath;
